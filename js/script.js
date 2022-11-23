f1a89d0636f2664719d248db46e2ec337ed1cf1b
Lukblan <luk.blanko@gmail.com>
2022-11-22 21:58:30 -0300

    Grid now is created based on user input

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-createDivs(35);
+addCleanEventToButton();
+addCreateEventToButton();
 
 function createDivs(number) {
   const sketchArea = document.querySelector("#sketch-area");
@@ -8,9 +9,40 @@ function createDivs(number) {
     for (let j = 0; j < number; j++) {
       let innerDiv = document.createElement("div");
       innerDiv.classList.add("innerDiv");
+      innerDiv.addEventListener("mouseover", () => {
+        innerDiv.style.backgroundColor = "black";
+      })
       newDiv.append(innerDiv)
     }
-
     sketchArea.append(newDiv)
   }
+}
+
+function replaceSketchArea() {
+  const sketchArea = document.querySelector("#sketch-area");
+  const newSketchArea = document.createElement("div");
+  newSketchArea.id = "sketch-area";
+  sketchArea.parentElement.replaceChild(newSketchArea,sketchArea);
+}
+
+function addCleanEventToButton() {
+  const cleanButton = document.querySelector("#clean");
+  const inputNumber = getUserGridSize();
+  cleanButton.addEventListener("click", () => {
+    replaceSketchArea()
+    createDivs(getUserGridSize());
+  });
+  createDivs(inputNumber);
+}
+
+function addCreateEventToButton() {
+  const createButton = document.querySelector("#create");
+  createButton.addEventListener("click", () => {
+    replaceSketchArea();
+    createDivs(getUserGridSize());
+  })
+}
+
+function getUserGridSize() {
+  return document.querySelector("input").value
 }

8475e2d1e11be24f0b4810e5251b9f34453a22f5
Lukblan <luk.blanko@gmail.com>
2022-11-26 20:52:52 -0300

    Implemented scroll bar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
+let gridSize = 32;
+let currentPosition;
+
 addCleanEventToButton();
 addCreateEventToButton();
 addBombEventToButton();
+moveBall()
 
 function createDivs(number) {
   if (number > 0 && number <= 100) {
@@ -33,12 +37,10 @@ function replaceSketchArea() {
 
 function addCleanEventToButton() {
   const cleanButton = document.querySelector("#eraser");
-  const inputNumber = getUserGridSize();
   cleanButton.addEventListener("click", () => {
     replaceSketchArea()
     createDivs(getUserGridSize());
   });
-  createDivs(inputNumber);
 }
 
 function addCreateEventToButton() {
@@ -56,4 +58,24 @@ function addBombEventToButton() {
 
 function getUserGridSize() {
   return document.querySelector("input").value
+}
+
+function moveBall() {
+  const ball = document.getElementById("ball");
+  ball.addEventListener("mousedown", (event) => {
+    currentPosition = event.clientX;
+    ball.addEventListener("mousemove", changeBallPosition)
+    window.addEventListener("mouseup", () => {
+      ball.removeEventListener("mousemove", changeBallPosition)
+    })
+  })
+}
+
+function changeBallPosition(event) {
+  let dist = event.clientX - currentPosition;
+  currentPosition = event.clientX;
+  let barMaxWidth = this.parentElement.getBoundingClientRect().left + this.parentElement.offsetWidth;
+  let ballRightPosition = Math.min(this.getBoundingClientRect().left + dist, barMaxWidth);
+  let ballLeftPosition = Math.max(ballRightPosition, this.parentElement.getBoundingClientRect().left)
+  this.style.left = ballLeftPosition + "px";
 }

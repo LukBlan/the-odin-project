be437d694d3c3a9437f3385b6e8116fe5b4bad52
Lukblan <luk.blanko@gmail.com>
2023-02-09 10:12:05 -0300

    Refactor: background color now apply to scores

--- js/round-score.js
+++ js/round-score.js
@@ -21,7 +21,18 @@
 
   function render() {
     playerScoreBox.innerText = playerScore;
+    playerScoreBox.className = getClassListByScore("player", playerScore, computerScore);
     computerScoreBox.innerText = computerScore;
+    computerScoreBox.className = getClassListByScore("computer", computerScore, playerScore);
+  }
+
+  function getClassListByScore(subject, score, rivalScore) {
+    return `score ${subject}-score ${getBackgroundColorByScore(score, rivalScore)}`;
+  }
+
+  function getBackgroundColorByScore(score, rivalScore) {
+    const result = (score > rivalScore)? "Win": (score < rivalScore)? "Lose": "Tie";
+    return backgroundColor.getBackgroundClassBy(result);
   }
 
   pubSub.subscribe("scoreChange", getResults);

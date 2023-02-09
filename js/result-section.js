416a73b8416c66141ca4c049bb6d15856fabd216
Lukblan <luk.blanko@gmail.com>
2023-02-08 23:05:30 -0300

    Refactor: Choices are now colored based on round result

--- js/result-section.js
+++ js/result-section.js
@@ -1,25 +1,44 @@
 (function resultSection() {
-  let playerChoice = null;
-  let computerChoice = null;
+  const backgroundColor = {
+    "paper-rock": "Win",
+    "paper-paper": "Tie",
+    "paper-scissors": "Lose",
+
+    "scissors-paper": "Win",
+    "scissors-scissors": "Tie",
+    "scissors-rock": "Lose",
+
+    "rock-scissors": "Win",
+    "rock-rock": "Tie",
+    "rock-paper": "Lose",
+  }
 
   // Cache DOM
   const playerChoiceImg = document.querySelector(".player-choice");
   const computerChoiceImg = document.querySelector(".computer-choice");
 
-  function setChoices(choiceObject) {
-    playerChoice = choiceObject.playerChoice;
-    computerChoice = choiceObject.computerChoice;
-    render()
+  function getChoices(choiceObject) {
+    const playerChoice = choiceObject.playerChoice;
+    const computerChoice = choiceObject.computerChoice;
+    render(playerChoice, computerChoice)
   }
 
-  function render() {
+  function render(playerChoice, computerChoice) {
     playerChoiceImg.src = generateSourceByChoice(playerChoice);
+    playerChoiceImg.className = `card player-choice ${getBackgroundColorClass(playerChoice, computerChoice)}`;
     computerChoiceImg.src = generateSourceByChoice(computerChoice);
+    computerChoiceImg.className = `card computer-choice ${getBackgroundColorClass(computerChoice, playerChoice)}`;
+  }
+
+  function getBackgroundColorClass(choice, rivalChoice) {
+    const result = backgroundColor[choice + "-" + rivalChoice];
+    return (result === "Win")? "green-background":
+              (result === "Lose")? "red-background": "yellow-background";
   }
 
   function generateSourceByChoice(choice) {
     return "./img/" + choice + ".png";
   }
 
-  pubSub.subscribe("newRound", setChoices)
+  pubSub.subscribe("newRound", getChoices)
 })()

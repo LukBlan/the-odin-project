a78ce5ccbd44775108d3ed2f90be259b488726d9
lukblan <luk.blanko@gmail.com>
2024-09-29 00:34:27 -0300

    Delete old js code

--- js/background-factory.js
+++
@@ -1,7 +0,0 @@
-const backgroundFactory = (function() {
-  function createBackgroundObject(elementReference, currentBg, newBg) {
-    return {elementReference: elementReference, currentBg: currentBg, newBg: newBg};
-  }
-
-  return {createBgObject: createBackgroundObject}
-})()
